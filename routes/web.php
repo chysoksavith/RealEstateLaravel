a1cd9b93bd6a7eb149c7f97dac4daa2aa1b1a086
chysoksavith <120388241+chysoksavith@users.noreply.github.com>
2023-11-23 15:40:47 +0700

    done custome login admin

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,6 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 Route::middleware(['auth', 'role:agent'])->group(function () {
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });
+
+// login admin
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
