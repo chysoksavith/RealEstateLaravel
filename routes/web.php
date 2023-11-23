29fd0e386af3ca807708b2573b785657f35c7cc0
chysoksavith <120388241+chysoksavith@users.noreply.github.com>
2023-11-23 15:19:32 +0700

    dont with admin logout

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,10 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+//
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 }); // end Group admin middleware
 
 Route::middleware(['auth', 'role:agent'])->group(function () {
